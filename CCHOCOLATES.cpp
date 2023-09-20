ad5da64f0d51229ce6e1f42003333e48be34d068
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-09-20 22:37:52 +0530

    Add files via upload

---
+++ CCHOCOLATES.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int t,x,y,z;
+	cin>>t;
+	cout<<"\n";
+	while(t--)
+	{
+	    cin>>x;
+	    cout<<"\n";
+	    cin>>y;
+	    cout<<"\n";
+	    cin>>z;
+	    cout<<"\n";
+	    cout<<((x*5+y*10)/z);
+	    cout<<"\n";
+	}
+	
+	return 0;
+}
