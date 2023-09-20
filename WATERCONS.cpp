38bc672080a506b0ae251d98ddae15f7a7b50db3
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-09-20 22:15:39 +0530

    Add files via upload

---
+++ WATERCONS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x,t;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x>=2000)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
