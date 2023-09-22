2fadd25cd9633c9f1e1bef5cb0e34b6ba4f2fcf4
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-09-22 20:10:55 +0530

    Add files via upload

---
+++ BATTERYLOW.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,X;
+    cin>>T;
+    while(T--)
+    {
+        cin>>X;
+        cout<<"\n";
+        if(X<=15)
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
