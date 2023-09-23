57e19ae928d0816bac4d6555b0aa5956f19d7b0b
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-09-23 17:26:54 +0530

    Add files via upload

---
+++ MINCARS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,N;
+	cin>>T;
+	cout<<endl;
+	while(T--)
+	{
+	    cin>>N;
+	    cout<<endl;
+	    if(N>4)
+	    cout<<((N+3)/4)<<endl;
+	    else
+	    cout<<1<<endl;
+	    
+	}
+	return 0;
+}
