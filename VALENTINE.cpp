8c0362f9e61f4759f621aebd17d92fb3da80cffa
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-09-27 20:02:36 +0530

    Add files via upload

---
+++ VALENTINE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        if(X<Y)
+        cout<<0<<endl;
+        else
+        cout<<(X/Y)<<endl;
+    }
+	return 0;
+}
