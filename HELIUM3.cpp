6901291073b1a4690d0d6afdba8a8295a0b6157e
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-10-01 12:52:53 +0530

    Add files via upload

---
+++ HELIUM3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int A,B,X,Y;
+	    cin>>A>>B>>X>>Y;
+	    if(A*B<=X*Y)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
