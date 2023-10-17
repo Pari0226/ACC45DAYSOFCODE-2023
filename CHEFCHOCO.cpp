0523792f4d8813704512fc9e39ea79f2dc81b15a
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-10-17 23:32:25 +0530

    Add files via upload

---
+++ CHEFCHOCO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int C, X, Y;
+	    cin>>C>>X>>Y;
+	    
+	    cout<<(C-X)*Y<<endl;
+	}
+	return 0;
+}
