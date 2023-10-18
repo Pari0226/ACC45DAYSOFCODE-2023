44bbc7adb2b4f63d552fb1db4787ca32413d4cf9
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-10-18 21:47:34 +0530

    Add files via upload

---
+++ NOTIME.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T[100], N,H,x;
+	cin>>N>>H>>x;
+	bool found = false;
+	for(int i = 0; i<N;i++){
+	    cin>>T[i];
+	    if(x+T[i] >= H){
+	        cout<<"YES"<<endl;
+	        found = true;
+	        break;
+	    }
+	}
+	if(found == false){
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
