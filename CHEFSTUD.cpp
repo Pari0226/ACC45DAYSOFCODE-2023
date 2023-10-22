83c48c5c040fd2c816aee0bbe39c2a10d0328210
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-10-22 11:27:58 +0530

    Add files via upload

---
+++ CHEFSTUD.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    int count = 0;
+	    for(int i=0;i<s.length();++i){
+	        if(s[i]=='>'){
+	            s[i] = '<';
+	        }
+	        else if(s[i]=='<'){
+	            s[i] = '>';
+	        }
+	    }
+	    for(int i=0;i<s.length()-1;++i){
+	        if(s[i]=='>' && s[i+1]=='<'){
+	            count++;
+	        }
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
