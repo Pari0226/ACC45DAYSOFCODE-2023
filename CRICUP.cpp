b47c34fe5a355e2b958b65dfb5b6225a0d6b89dc
Pari Singh <141944800+Pari0226@users.noreply.github.com>
2023-11-03 01:38:58 +0530

    Add files via upload

---
+++ CRICUP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,d;
+	    cin>>x>>y>>d;
+	    if(abs(x-y)<=d){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
---
+++ FRIMEET.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t-->0)
+	{
+	    int x1,x2;
+	    cin>>x1>>x2;
+	    if(x1>=x2)
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+}
